1e87c1dc3fe74f79ddb5df228750dd5b9ad8d0ab
Gautam Kumar <kumargautamsp@gmail.com>
2024-10-15 19:40:44 +0530

    commit

--- hotel-react/src/data.js
+++ hotel-react/src/data.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 async function fetchData() {
   try {
-    const response = await axios.get(`${process.env.BACKEND_URL}/api/data`);
-    console.log("Data fetched successfully:");
+    console.log(process.env.BACKEND_URL)
+    const backendUrl = process.env.BACKEND_URL || 'https://hotel-dashhboard.onrender.com';
+    const response = await axios.get(`${backendUrl}/api/data`);
+   
+    console.log("Data fetched successfully:",response.data);
+
     return response.data;
   } catch (error) {
     console.error("Error fetching data from API:", error);
@@ -12,3 +16,4 @@ async function fetchData() {
 }
 
 export default fetchData;
+
